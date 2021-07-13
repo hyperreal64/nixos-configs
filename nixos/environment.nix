1297abf6f98328c48424bbd78bd2abb3cc3b5ff9
Jeffrey Serio <hyperreal64@gmail.com>
2021-07-13 18:43:59 -0500

    Update

---
+++ nixos/environment.nix
@@ -0,0 +1,73 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+{
+    nixpkgs.config.allowUnfree = true;
+
+    environment.shells = [ pkgs.bashInteractive pkgs.zsh ];
+
+    nixpkgs.overlays = [
+        (import (let
+            #rev = "master";
+            rev = "2aff1c00dc619b5b8af3dee95461b41f3a6fd7f0";
+          in
+          builtins.fetchTarball {
+              url = "https://github.com/nix-community/neovim-nightly-overlay/archive/${rev}.tar.gz";
+          })
+        )
+    ];
+
+    environment.systemPackages = with pkgs; [
+        brave
+        firefox
+        git
+        gnupg
+        gopls
+        harfbuzzFull
+        ldns
+        mmc-utils
+        neovim-nightly
+        nix-diff
+        nix-doc
+        nix-du
+        nix-top
+        nix-tree
+        nodePackages.bash-language-server
+        nodePackages.dockerfile-language-server-nodejs
+        nodePackages.pyright
+        nodePackages.vscode-json-languageserver-bin
+        nodePackages.yaml-language-server
+        rnix-lsp
+        sshfs
+        system76-firmware
+        tree-sitter
+        wget
+        zsh-nix-shell
+    ];
+
+    nix.extraOptions = ''
+        keep-outputs = true
+        keep-derivations = true
+    '';
+
+    # sshfs
+    programs.fuse.userAllowOther = true;
+
+    # GnuPG Agent
+    programs.gnupg = {
+        agent.enable = true;
+        agent.pinentryFlavor = "gnome3";
+        dirmngr.enable = true;
+    };
+
+    # Enable Zsh
+    programs.zsh.enable = true;
+
+    # Flatpak
+    services.flatpak.enable = true;
+    xdg.portal.enable = true;
+}
+
---
+++ nixos/xfce.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, callPackage, ... }:
+
+{
+    services.xserver = {
+        enable = true;
+        desktopManager = {
+            xterm.enable = false;
+            xfce.enable = true;
+        };
+        displayManager = {
+            gdm.enable = true;
+            defaultSession = "xfce";
+        };
+    };
+
+    services.xserver.desktopManager.xfce = {
+        thunarPlugins = [ pkgs.xfce.thunar-archive-plugin pkgs.xfce.thunar-volman ];
+    };
+
+    environment.systemPackages = with pkgs; [
+        dmenu
+        pavucontrol
+        qalculate-gtk
+        ranger
+        rofi
+        rofi-file-browser
+        rofi-power-menu
+        sxhkd
+        xdotool
+        xfce.xfce4-genmon-plugin
+        xfce.xfce4-weather-plugin
+        xfce.xfce4-clipman-plugin
+        xfce.xfce4-battery-plugin
+        xorg.xset
+        xorg.xsetroot
+        zathura
+    ];
+}
